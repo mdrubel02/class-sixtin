f8e2ca436a535d8d68dfff0d9f6975d744719ae7
mdrubel02 <mdrubel072125@gmail.com>
2022-02-25 23:59:49 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log('connenting');
+
+// 01 eita ke literal object bole
+const student={name: 'rubel mia', job:'web-devolper'};
+
+
+// 02 eita ke constructor object bole
+const person= new Object();
+
+
+const hero='alom';
