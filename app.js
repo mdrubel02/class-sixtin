e5b138b32f1cf220cddf91c7c15cf47ef91b6b16
mdrubel02 <mdrubel072125@gmail.com>
2022-02-26 16:30:42 +0600

    added

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 // console.log('connenting');
 
 // 01 eita ke literal object bole
-const student={name: 'rubel mia', job:'web-devolper'};
+// const student={name: 'rubel mia', job:'web-devolper'};
 
 
 // 02 eita ke constructor object bole
-const person= new Object();
+// const person= new Object();
 
 
-const human=Object.create(student);
+// const human=Object.create(student);
 
-console.log(human.name);
+// console.log(human.name);
 
 
 class pepole{
@@ -22,3 +22,17 @@ class pepole{
 
 const pepol=new pepole('piyas', 22);
 console.log(pepol);
+
+
+class studentDetiles {
+    constructor(age, height, id, subject, year) {
+        this.age = age;
+        this.height = height;
+        this.id = id;
+        this.subject = subject;
+        this.year = year;
+    }
+};
+
+const student=new studentDetiles(23,4.6,78,'bangla',2020);
+console.log(student);
